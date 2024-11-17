1efbdf836612a6be04f5cb76b916acf9e4bc0748
njomzaarexhepii@gmail.com <75095687+NjomzaRexhepi@users.noreply.github.com>
2024-11-17 20:01:09 +0100

    week class for groups

---
+++ SocialGolfersProblem/SocialGolfersProblem/Week.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialGolfersProblem
+{
+    public class Week
+    {
+        public List<GroupOfPlayers> Groups { get; set; }
+        
+        public Week(int numGroups) {
+            Groups = new List<GroupOfPlayers>(numGroups);
+
+            for(int i = 0; i<numGroups; i++)
+            {
+                Groups.Add(new GroupOfPlayers());
+            }
+        }
+    }
+}
