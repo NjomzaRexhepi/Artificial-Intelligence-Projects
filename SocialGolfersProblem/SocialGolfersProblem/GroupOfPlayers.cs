618bd9fe9e0221062b9cb7949d10536384df3558
njomzaarexhepii@gmail.com <75095687+NjomzaRexhepi@users.noreply.github.com>
2024-11-17 15:12:52 +0100

    Group of players class

---
+++ SocialGolfersProblem/SocialGolfersProblem/GroupOfPlayers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialGolfersProblem
+{
+    public class GroupOfPlayers
+    {
+        public List<int> Players { get; set; }
+        public GroupOfPlayers()
+        {
+            Players = new List<int>();
+        }
+    }
+}
