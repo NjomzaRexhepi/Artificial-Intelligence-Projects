5ec0aada85b8b0c747aabee451f885782d315d5e
Kaltrinë Baliu <kaltrine.baliu@student.uni-pr.edu>
2024-11-20 14:39:16 +0100

    added the function to solve a sudoku

--- SudokuSolver/Sudoku.py
+++ SudokuSolver/Sudoku.py
@@ -17,3 +17,24 @@ def is_valid_move(grid, row, col, num):
                 return False
     
     return True
+
+def bfs_backtracking_sudoku(grid):
+    queue = deque([(grid, 0, 0)])  
+    while queue:
+        current_grid, row, col = queue.popleft()
+
+        if row == 9:  
+            return current_grid
+        
+        if current_grid[row][col] != 0: 
+            next_row, next_col = (row, col + 1) if col < 8 else (row + 1, 0)
+            queue.append((current_grid, next_row, next_col))
+        else:
+            for num in range(1, 10):
+                if is_valid_move(current_grid, row, col, num):
+                    new_grid = [r[:] for r in current_grid]  
+                    new_grid[row][col] = num
+                    next_row, next_col = (row, col + 1) if col < 8 else (row + 1, 0)
+                    queue.append((new_grid, next_row, next_col))
+    
+    return None 
