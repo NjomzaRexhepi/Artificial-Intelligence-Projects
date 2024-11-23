98a09d314e71609e6442e213d3632c31ff0f0aa8
erblinaberisha <Ereblina.Berisha@student.uni-pr.edu>
2024-11-24 00:47:29 +0100

    Randomization in Latin square

--- LatinSquareProblem/Program.cs
+++ LatinSquareProblem/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class LatinSquare
 {
@@ -14,6 +15,7 @@ class LatinSquare
         colUsed = new bool[n, n + 1];
     }
 
+    // Solves the Latin square using backtracking and random placement
     public bool SolveSquare()
     {
         return Backtrack(0, 0);
@@ -21,27 +23,47 @@ class LatinSquare
 
     private bool Backtrack(int row, int col)
     {
-        if (row == n) return true; // Base case
+        if (row == n) return true; // Base case: All rows are filled
 
+        // Calculate the next cell
         int nextRow = col == n - 1 ? row + 1 : row;
         int nextCol = col == n - 1 ? 0 : col + 1;
 
+        // Create a random list of numbers to try
+        List<int> candidates = new List<int>();
         for (int num = 1; num <= n; num++)
         {
             if (!rowUsed[row, num] && !colUsed[col, num])
-            {
-                square[row, col] = num;
-                rowUsed[row, num] = colUsed[col, num] = true;
-
-                if (Backtrack(nextRow, nextCol))
-                    return true;
-
-                square[row, col] = 0;
-                rowUsed[row, num] = colUsed[col, num] = false;
-            }
+                candidates.Add(num);
         }
 
-        return false;
+        // Randomly shuffle the candidates
+        Random rng = new Random();
+        for (int i = candidates.Count - 1; i > 0; i--)
+        {
+            int j = rng.Next(i + 1);
+            int temp = candidates[i];
+            candidates[i] = candidates[j];
+            candidates[j] = temp;
+        }
+
+        // Try placing the numbers in a random order
+        foreach (var num in candidates)
+        {
+            // Place the number and mark it used
+            square[row, col] = num;
+            rowUsed[row, num] = colUsed[col, num] = true;
+
+            // Recurse with the next cell
+            if (Backtrack(nextRow, nextCol))
+                return true;
+
+            // Undo the placement if it didn't work
+            square[row, col] = 0;
+            rowUsed[row, num] = colUsed[col, num] = false;
+        }
+
+        return false; // No valid candidate found, backtrack
     }
 
     public void PrintSquare()
